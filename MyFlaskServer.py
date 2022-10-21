e5f2e1bd835530c6b3f0193fe368332852b0f5cc
yogev shoshan <yogev1520@gmail.com>
2022-10-21 22:35:14 +0300

    Flask_first_commit

---
+++ MyFlaskServer.py
@@ -0,0 +1,13 @@
+from flask import Flask,redirect,url_for
+
+app= Flask(__name__)#create instance of flask
+
+@app.route("/")
+def index():
+    return "Index Page"# main page or index page >>>  http://127.0.0.1:5000 <<<
+    """here you can put content for main page """
+
+@app.route('/hello')# hello page >>> http://127.0.0.1:5000\hello <
+def hello_name():
+    return "hello.world!"
+    """ here you can put content to the page"""
